00c3f0f891be9e9f1bf7ccbea5788799437392fd
Jon Stødle <jonstodle@outlook.com>
2014-01-10 23:48:20 +0100

    Add PocketAddItem for adding new urls

--- PocketInterface/PocketAddItem.cs
+++ PocketInterface/PocketAddItem.cs
@@ -1,10 +1,49 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace PocketInterface {
     public class PocketAddItem {
+        private string _consumerKey;
+        private string _accessToken;
+        private string _url;
+        private string _title;
+        private string _tags;
+        private string _tweetId;
+
+        public PocketAddItem(string ConsumerKey, string AccessToken, string Url, string Title = null, string Tags = null, string TweetId = null) {
+            _consumerKey = ConsumerKey;
+            _accessToken = AccessToken;
+            _url = Url;
+            _title = Title;
+            _tags = Tags;
+            _tweetId = TweetId;
+        }
+
+        public string GetJsonString() {
+            var json = new JObject();
+            json.Add("url", _url);
+            if(_title != null)      json.Add("title", _title);
+            if(_tags != null)       json.Add("tags", _tags);
+            if(_tweetId != null)    json.Add("tweet_id", _tweetId);
+            json.Add("consumer_key", _consumerKey);
+            json.Add("access_token", _accessToken);
+            return json.ToString();
+        }
+
+        public string GetEncodedJsonString() {
+            var json = new JObject();
+            json.Add("url", WebUtility.UrlEncode(_url));
+            if(_title != null) json.Add("title", _title);
+            if(_tags != null) json.Add("tags", _tags);
+            if(_tweetId != null) json.Add("tweet_id", _tweetId);
+            json.Add("consumer_key", _consumerKey);
+            json.Add("access_token", _accessToken);
+            return json.ToString();
+        }
     }
 }
